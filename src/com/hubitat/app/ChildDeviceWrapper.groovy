2b9be96e77a50866c7209bb7188e37b391abee91
Grayson Carr <gcarr@barracuda.com>
2020-02-27 11:11:06 -0500

    Fix: Parent and child device wrappers should extend DeviceWrapper, not Device

--- src/com/hubitat/app/ChildDeviceWrapper.groovy
+++ src/com/hubitat/app/ChildDeviceWrapper.groovy
@@ -4,5 +4,5 @@ package com.hubitat.app
  * https://docs.hubitat.com/index.php?title=Device_Object
  * https://docs.smartthings.com/en/latest/ref-docs/device-ref.html
  */
-abstract class ChildDeviceWrapper extends Device {
+abstract class ChildDeviceWrapper extends DeviceWrapper {
 }
--- src/com/hubitat/app/ParentDeviceWrapper.groovy
+++ src/com/hubitat/app/ParentDeviceWrapper.groovy
@@ -4,5 +4,5 @@ package com.hubitat.app
  * https://docs.hubitat.com/index.php?title=Device_Object
  * https://docs.smartthings.com/en/latest/ref-docs/device-ref.html
  */
-abstract class ParentDeviceWrapper extends Device {
+abstract class ParentDeviceWrapper extends DeviceWrapper {
 }
