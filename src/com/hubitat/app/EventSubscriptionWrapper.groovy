1a383a67cdd5c7b62e55b3e48f30e26af6461833
Grayson Carr <gcarr@barracuda.com>
2020-03-01 01:33:47 -0500

    Added EventSubscriptionWrapper properties

--- src/com/hubitat/app/EventSubscriptionWrapper.groovy
+++ src/com/hubitat/app/EventSubscriptionWrapper.groovy
@@ -1,4 +1,14 @@
 package com.hubitat.app
 
 abstract class EventSubscriptionWrapper {
+
+    // All properties are undocumented
+
+    Long id
+    Long locationId
+    Long deviceId
+    DeviceWrapper device
+    // Attribute name
+    String data
+    String handler
 }
